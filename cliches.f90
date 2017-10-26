797473315b22059443dcd5d17e5b5eeb000915d5
cjanzen <cjanzen@users.noreply.github.com>
2017-10-25 19:33:30 -0500

    Added new cliche
    #6 Added "Anywhere Is Within Walking Distance If You Have The Time Axiom"

--- cliches.f90
+++ cliches.f90
@@ -4,6 +4,16 @@ INTEGER :: Choice, Roll
 
 Choice = Roll(195)
 
-! Removed for copyright reasons
+! Original versions removed for copyright reasons
+! New versions created from the fevered brain of devICTers...
+
+CASE (1)
+WRITE (*,'(1X,A)') "Anywhere Is Within Walking Distance If You Have The Time Axiom"
+WRITE (*,('1X,A)') "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+WRITE (*,'(1X,A)') "Immediately after finding a potion, spell or gem of transportation, a wizened "
+WRITE (*,'(1X,A)') "oldster appears and wearily states 'That's all well and good, but anywhere is "
+WRITE (*,'(1X,A)') "within walking distance if you have the time'. When the character resumes the "
+WRITE (*,'(1X,A)') "quest, he or she discovers the oldster has stolen the key to the transport    "
+WRITE (*,'(1X,A)') "item and disappeared.                                                         "
 
 END SUBROUTINE Cliches
