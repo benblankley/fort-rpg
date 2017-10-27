fde7e4fb5177a025d1f52b179d941c0b63875c5a
cjanzen <cjanzen@users.noreply.github.com>
2017-10-27 18:21:37 -0500

    Update generalstore.f90
    Added new item: weapon.
    
    Note: I'm not sure Line 47 is correct, however.

--- generalstore.f90
+++ generalstore.f90
@@ -12,15 +12,18 @@ WRITE (*,*) "General Store"
 WRITE (*,*) "-----------------------"
 WRITE (*,*)
 WRITE (*,*) "GROCER: Tonics and potions are 50 and 100 GP, respectively. "
+WRITE (*,*) "        Weapons are 150 GP. "
 WRITE (*,*) "        Here is what you have right now:"
 WRITE (*,*) 
 WRITE (*,'(1X,A,I4,T20,A,I4)') "Tonics:", Backpack(1)
 WRITE (*,'(1X,A,I4,T20,A,I4)') "Potions:", Backpack(2)
+WRITE (*,'(1X,A,I4,T20,A,I4)') "Weapons:", Backpack(3)
 WRITE (*,*)
 WRITE (*,'(1X,A,I7,A)') "GROCER: Which item would you like to buy? (You have ",Backpack(3)," GP.)"
 WRITE (*,*)
 WRITE (*,'(1X,A,T20,A)') "(T)onic"
 WRITE (*,'(1X,A,T20,A)') "(P)otion"
+WRITE (*,'(1X,A,T20,A)') "(W)eapon"
 WRITE (*,*) "(N)othing"
 READ (*,*) Choice
 
@@ -34,6 +37,13 @@ IF (Choice == "T" .OR. Choice == "t") THEN
 ELSE IF (Choice == "P" .OR. Choice == "p") THEN
  IF (Backpack(3) >= 100) THEN
   Backpack(3)=Backpack(3)-100
+  Backpack(2)=Backpack(2)+1
+ ELSE
+  WRITE (*,*) "GROCER: You do not have enough money."
+ END IF
+ELSE IF (Choice == "W" .OR. Choice == "w") THEN
+ IF (Backpack(3) >= 150) THEN
+  Backpack(3)=Backpack(3)-150
   Backpack(2)=Backpack(2)+1
  ELSE
   WRITE (*,*) "GROCER: You do not have enough money."
